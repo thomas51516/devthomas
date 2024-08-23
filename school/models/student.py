302f82f6b51de871d429a4650a2b073774310f0f
thomas51516 <thomas51516@gmail.com>
2024-08-23 00:47:44 +0000

    Add files via upload

--- school/models/student.py
+++ school/models/student.py
@@ -4,13 +4,15 @@ from datetime import date
 class SchoolStudent(models.Model):
 
     _name = 'school.student'
-    _inherit = ['mail.thread','mail.activity.mixin']
+    _inherit= ['mail.thread','mail.activity.mixin']
     _description = 'Student'
 
     name = fields.Char(string="Nom de famille", required=True, 
-    track_visibility='always'
+    track_visibility='onchange'
     )
-    birth_date = fields.Date(string='Date de naissance', default=date.today())
+    birth_date = fields.Date(string='Date de naissance', 
+    track_visibility='onchange',
+     default=date.today())
     photo = fields.Binary(string="Photo")
     description = fields.Html()
     is_former_student = fields.Boolean(string="Est est ancien élève")
@@ -18,60 +20,67 @@ class SchoolStudent(models.Model):
         ('Masculin', 'Masculin'),
         ('Féminin','Féminin'),
         ('Autre','Autre')
-    ])
-    age = fields.Integer(string="Age", compute='_compute_age')
+    ], 
+    track_visibility='onchange'
+    )
+    age = fields.Integer(string="Age", 
+    track_visibility='onchange',
+     store=True, compute="_compute_age")
     note = fields.Float(string="Note")
     class_id = fields.Many2one(
         'student.class',
         string='Classe',
     )
 
-    progession = fields.Float(string="Progression", default=1)
-
     course_ids = fields.Many2many('school.course', string="Cours")
     note_ids = fields.One2many('student.note', 'student_id')
 
-    priority = fields.Selection([
-        ('1', 'Faible'),
-        ('2','Moyen'),
-        ('3','Normal'),
-        ('4','Elevé'),
-        ('5','Très élevé')
-    ], 
+    email = fields.Char(string="E-mail", 
     track_visibility='onchange'
     )
+    phone = fields.Char(string="Téléphone", 
+    track_visibility='onchange'
+    )
+
+    progression = fields.Float(string="Progression", default=23)
+    course_time = fields.Float(string="Heure de cours")
+
+    priority = fields.Selection([
+        ('moyen', 'Moyen'),
+        ('normal','Norml'),
+        ('eleve','Elevé'),
+        ('Tres eleve','Très elevé')
+    ], default="eleve")
 
     state = fields.Selection([
         ('preinscription', 'Préinscription'),
-        ('inscription', 'Inscription'),
+        ('inscription','Inscription'),
         ('abandonne','Abandonné'),
-    ], default="preinscription", 
-    track_visibility='onchange'
-    )
+        ('exclu','Exclu')
+    ], default='preinscription')
 
-    def abandonner(self):
-        for el in self:
-            el.state = 'abandonne'
+    def compute_preinscription(self):
+        self.state = 'preinscription'
 
-    def inscrire(self):
-        for el in self:
-            el.write({
-                'state':'inscription'
-            })
-
-    def compute_progression(self):
-        self.progession += 5
+    def compute_excul(self):
+        self.state = 'exclu'
 
 
-    def compute_pro(self):
-        self.progession -=5
+    def compute_abandonne(self):
+        self.state = 'abandonne'
+
+    def compute_inscription(self):
+        self.state = 'inscription'
+
+    def compute_progress(self):
+        self.progression +=  5
 
     @api.onchange('birth_date')
+    @api.depends('birth_date','age')
     def _compute_age(self):
-        for el in self:
-            age  = 0
-            if el.birth_date:
-                today = date.today()
-                birth_date = el.birth_date
-                age = today.year - birth_date.year
-            el.age = age
+        age = 0
+        if self.birth_date:
+            today = date.today()
+            birth_date = self.birth_date
+            age = today.year - birth_date.year
+        self.age = age
